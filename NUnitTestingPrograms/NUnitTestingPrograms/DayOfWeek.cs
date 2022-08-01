898a346e3a35e950352528c002971bbe065578c2
akshayb407 <akshayb407@gmail.com>
2022-08-01 17:57:09 +0530

    DayOfWeek

---
+++ NUnitTestingPrograms/NUnitTestingPrograms/DayOfWeek.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace NUnitTestingPrograms
+{
+    internal class DayOfWeek
+    {
+        public DayOfWeek()
+        {
+            Console.WriteLine("enter the month date and year..");
+
+
+             Console.WriteLine(GetWeekNumber(Convert.ToInt32('d'), Convert.ToInt32('m'),Convert.ToInt32('y')));
+                                  
+        }
+
+        public String GetWeekNumber(int d, int m, int y)
+        {
+
+            int intTemp = Convert.ToInt32(Console.ReadLine());
+            String[] array = {"Sunday","Monday","Tuesday","Wednesday","Thursday",
+                    "Friday","Saturday" };
+            int yf = y - (14 - m) / 12;
+            int x = yf + yf / 4 - yf / 100 + yf / 400;
+            int mf = m + 12 * ((14 - m) / 12) - 2;
+            return array[((d + x + 31 * (mf / 12)) % 7)];
+        }
+
+    }
+}
--- NUnitTestingPrograms/NUnitTestingPrograms/Program.cs
+++ NUnitTestingPrograms/NUnitTestingPrograms/Program.cs
@@ -7,9 +7,16 @@ namespace NUnitTestingPrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To NUnit Testing Programs!");
-            Console.WriteLine("Wending Machine program!");
-            VendingMachine vm = new VendingMachine();
-            vm.GiveChange(0);
+            // Console.WriteLine("Wending Machine program!");
+            //VendingMachine vm = new VendingMachine();
+            //vm.GiveChange(0);
+
+            Console.WriteLine("Welcome To Day Of Week Program!");
+            DayOfWeek dayOfWeek = new DayOfWeek();
+           
+            
+         
+            
         }
     }
 }
