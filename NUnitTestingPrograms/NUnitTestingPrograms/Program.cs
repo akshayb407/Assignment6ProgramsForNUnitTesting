80b27a592df9b0db9df038708a143014e5655afb
akshayb407 <akshayb407@gmail.com>
2022-08-01 22:30:58 +0530

    SqrtProgram

--- NUnitTestingPrograms/NUnitTestingPrograms/Program.cs
+++ NUnitTestingPrograms/NUnitTestingPrograms/Program.cs
@@ -19,9 +19,13 @@ namespace NUnitTestingPrograms
             //tc.FahrenheitToC(0);
             //tc.CelsiusToF(0);
 
-            Console.WriteLine("Monthly Payment Program!");
-            MonthlyPayment mp = new MonthlyPayment();
-            mp.Payment('p','y','r');
+            //Console.WriteLine("Monthly Payment Program!");
+            //MonthlyPayment mp = new MonthlyPayment();
+            //mp.Payment('p','y','r');
+
+            Console.WriteLine("Sqrt Program!");
+            Sqrt s = new Sqrt();
+            s.Sqroot('0');
             
          
             
---
+++ NUnitTestingPrograms/NUnitTestingPrograms/Sqrt.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTestingPrograms
+{
+    internal class Sqrt
+    {
+        public Sqrt()
+        {
+            Console.Write("enter the number for its SquareRoot:");
+            
+            Console.WriteLine("The Square root is : " + Sqroot('t'));
+        }
+        public double Sqroot(double t)
+        {
+            double c = t;
+            double epsilon = 1e-15;
+            while (Math.Abs(t - c / t) > epsilon * t)
+            {
+                t = (c / t + t) / 2;
+            }
+            return t;
+        }
+    }
+}
